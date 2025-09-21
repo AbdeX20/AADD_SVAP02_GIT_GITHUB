dff310da593cbad93eb7cd72b57f1c8ac2ec3e9e
Abdellah Amaddaou <abdellah.amaddaou@educa.madrid.org>
2025-09-21 18:58:00 +0200

    Inicio del proyecto

---
+++ src/es/ciudadescolar/Programa.java
@@ -0,0 +1,8 @@
+package es.ciudadescolar;
+public class Programa
+{
+    public static void main(String[] args) throws Exception
+    {
+        System.out.println("Empezamos a usar Repositorios de Código…");
+    }
+}
