7b8262b2b6ba518479d6bfe1bf2fc2c73a9e807e
Abdellah Amaddaou <abdellah.amaddaou@educa.madrid.org>
2025-09-21 19:09:05 +0200

    Nueva Clase y Modificación Programa

--- src/es/ciudadescolar/Programa.java
+++ src/es/ciudadescolar/Programa.java
@@ -1,8 +1,12 @@
 package es.ciudadescolar;
+
+import es.ciudadescolar.utils.MessageManager;
+
 public class Programa
 {
     public static void main(String[] args) throws Exception
     {
-        System.out.println("Empezamos a usar Repositorios de Código…");
+        //System.out.println("Empezamos a usar Repositorios de Código…");
+        System.out.print(MessageManager.SALUDO);
     }
 }
---
+++ src/es/ciudadescolar/utils/MessageManager.java
@@ -0,0 +1,6 @@
+package es.ciudadescolar.utils;
+
+public class MessageManager
+{
+    public static final String SALUDO = "Empezamos a usar Repositorios de Código…";
+}
